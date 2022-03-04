3b9b514d15d40638cb6fef0c1e0661e9169c3921
Denilson da Silva Pereira <denilsongunner@gmail.com>
2022-03-04 12:31:58 -0300

    exercícios 11 e 12

---
+++ exercicios/ex012/hour.js
@@ -0,0 +1,12 @@
+let agora = new Date()
+let hora = agora.getHours( )
+console.log(`Agora são exatamente ${hora} horas.`)
+if(hora>5 && hora<12){
+    console.log('Bom dia!')
+}else if(hora>12 && hora<18){
+    console.log('Boa tarde!')
+}else if(hora>18 || hora<00){
+    console.log('Boa Noite!')
+}else{
+    console.log('Boa Madrugada!')
+}
