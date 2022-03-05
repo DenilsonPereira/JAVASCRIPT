6049cc389e38818d15d0eb01e36f13e9aba2ab52
Denilson da Silva Pereira <denilsongunner@gmail.com>
2022-03-04 22:40:39 -0300

    finish ex013

---
+++ exercicios/ex013/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `A hora de agora é ${hora} horas.`
+
+    if(hora >= 0 && hora <= 12){
+        //Bom dia
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = '#F9CA34'
+    }else if(hora > 12 && hora <= 18){
+        //Boa tarde
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = '#AA4414'
+    }else{
+        //Boa Noite
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = '#342B48'
+    }
+}
