32e314136fcae0e673e7cf2d3c05a6e32b63f4ab
Denilson da Silva Pereira <denilsongunner@gmail.com>
2022-03-08 11:40:02 -0300

    Create ambiente.js

---
+++ exercicios/ex015/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+
+while (c<6){
+    console.log('no')
+    c++
+}
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
