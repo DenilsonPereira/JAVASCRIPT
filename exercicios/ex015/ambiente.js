eeb60646b045e4cce60390753cbf5c747d1db059
Denilson da Silva Pereira <denilsongunner@gmail.com>
2022-03-08 12:00:37 -0300

    Update ambiente.js

--- exercicios/ex015/ambiente.js
+++ exercicios/ex015/ambiente.js
@@ -1,11 +1,11 @@
-//Usada e correta também
+//Teste feito no inicio
 var c = 1
 do{
     console.log(`Passo ${c}`)
     c++
 } while (c<=6)
 
-//Mais usada
+//Teste feita no final(mais usada)
 var c = 1
 while (c<=6){
     console.log(`Passo ${c}`)
