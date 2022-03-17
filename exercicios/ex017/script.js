78ee0548cbc666a4789aafa7f6f324926f1a8e80
Denilson da Silva Pereira <denilsongunner@gmail.com>
2022-03-17 20:15:08 -0300

    finish ex0017

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,31 +1,20 @@
 function gerar(){
-    
-
     let num = window.document.getElementById('txtnum')
     let res = window.document.getElementById('res')
-    let n = Number(num.value)
-    if(n>0 && n<=9){
-        //window.alert('Funcionando')
-        let a = n * 1
-        let b = n * 2
-        let c = n * 3
-        let d = n * 4
-        let e = n * 5
-        let f = n * 6
-        let g = n * 7
-        let h = n * 8
-        let i = n * 9
-
-            res.innerHTML = `${n} x 1 = ${a}<br>
-                             ${n} x 2 = ${b} <br>
-                             ${n} x 3 = ${c} <br>
-                             ${n} x 4 = ${d} <br>
-                             ${n} x 5 = ${e} <br>
-                             ${n} x 6 = ${f} <br>
-                             ${n} x 7 = ${g} <br>
-                             ${n} x 8 = ${h} <br>
-                             ${n} x 9 = ${i}`
+    //se input igual a vazio, pede para digitar um número
+    if(num.value.length == 0){
+        window.alert('Digite um número por favor!')
     }else{
-        window.alert('ERRO')
+        //Realiza a multiplicação se input diferente de vazio
+        let n = Number(num.value)
+        let c = 1
+        res.innerHTML = ''
+        while(c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `res${c}`
+            res.appendChild(item)
+            c++
+        }
     }
 }
